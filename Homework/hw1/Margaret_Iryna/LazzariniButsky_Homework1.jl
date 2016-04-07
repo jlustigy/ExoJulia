cdad4329281197373d287ea1c1d776ed8debd132
ibutsky <iryna.butsky@gmail.com>
2016-04-07 15:11:53 -0700

    adding homework 1a

---
+++ Homework/hw1/Margaret_Iryna/LazzariniButsky_Homework1.jl
@@ -0,0 +1,14 @@
+#@stest [[kepler_solve!(M,ecc) for M in linspace(0,2pi,100)] for ecc in linspace(0,0.999,100)]
+
+function Kepler(M, e)
+    E_old = M
+    epsilon = 1.0
+    while epsilon > 1.0E-12
+        g = E_old - e*sin(E_old) - M
+        g_prime = 1.0 - e*cos(E_old)
+        E_new = E_old - (g/g_prime)
+        epsilon = abs(E_old - E_new)/E_old
+        E_old = E_new
+    end
+    return E_old
+end
