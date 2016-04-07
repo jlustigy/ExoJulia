96575f2a3d7628ce04e6b0f521bb71682e109c0b
olehmer <olehmer@D-172-28-23-185.dhcp4.washington.edu>
2016-04-07 11:45:00 -0700

    Updated stest for homework 1

--- Homework/hw1/Owen_Hannah_Matt/KeplersEquation.jl
+++ Homework/hw1/Owen_Hannah_Matt/KeplersEquation.jl
@@ -12,6 +12,28 @@ Return
 f   The true anomaly [0<f<2pi] on success. -1 on failure
 =#
 function TrueAnomaly(e, m, t)
+  #tan(f/2)=sqrt((1+e)/(1-e))*tan(ea/2)
+  ea = EccentricAnomaly(e, m, t)
+  if ea == -1
+    return ea
+  end
+  f = atan2(sqrt(1-e^2)*sin(ea),cos(ea)-e)
+end
+
+#=
+EccentricAnomaly()
+
+Solve Kepler's equation for the true anomaly using Newton's method.
+
+Input
+e   The eccentricity of the orbit [0<e<1]
+m   The mean anomaly in radians [0<m<2pi]
+t   The tolerance desired for this calculation in decimal format [i.e. 0.00001]
+
+Return
+f   The eccentric anomaly [0<ea<2pi] on success. -1 on failure
+=#
+function EccentricAnomaly(e, m, t)
   ea = m+0.85*e*sign(sin(m)) #initial guess of eccentric anomaly
   d = ea - e*sin(ea) - m
 
@@ -27,10 +49,9 @@ function TrueAnomaly(e, m, t)
   f = -1
   if num_iter < iter_limit
     #success
-    #tan(f/2)=sqrt((1+e)/(1-e))*tan(ea/2)
-    f = atan2(sqrt(1-e^2)*sin(ea),cos(ea)-e)
+    return ea
   end
   return f
 end
 
-#@stest TrueAnomaly(0.5, 27*pi/180, 0.00001)
+#@stest [[EccentricAnomaly(ecc,M,0.00001) for M in linspace(0,2pi,100)] for ecc in linspace(0,0.999,100)]
