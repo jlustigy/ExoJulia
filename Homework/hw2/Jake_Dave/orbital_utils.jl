9e1342ea3b1a9c8323f95d81030756a040716589
Jacob Lustig-Yaeger <jlustigy@uw.edu>
2016-04-11 16:38:20 -0700

    J-D working with Matricies

--- Homework/hw2/Jake_Dave/orbital_utils.jl
+++ Homework/hw2/Jake_Dave/orbital_utils.jl
@@ -6,18 +6,18 @@
 ######################
 
 
-function mean_anomaly!(P::Float64, t::Float64, t0::Float64)
+function mean_anomaly(P::Float64, t::Float64, t0::Float64)
     (2.0pi / P) * (t-t0)
 end
 
-function f_from_E!(E::Float64, ecc::Float64)
+function f_from_E(E::Float64, ecc::Float64)
     2.0 * atan2(sqrt(1.0 - ecc) * cos(E/2.0), sqrt(1.0 + e) * sin(E/2.0))
 end
 
-function r_from_f!(a::Float64, ecc::Float64, f::Float64)
+function r_from_f(a::Float64, ecc::Float64, f::Float64)
     a * (1.0 - ecc*ecc) / (1.0 - ecc*cos(f))
 end
 
-function f_from_M!(ecc::Float64, M::Float64)
+function f_from_M(ecc::Float64, M::Float64)
     f_from_E!(ExoJulia.Orbit.kepler_solve!(M, ecc), ecc)
 end
