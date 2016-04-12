b45d4c19b56a25d572335078710bc3e459abf649
Owen Lehmer <olehmer@uw.edu>
2016-04-12 13:17:21 -0700

    hw2 updated

--- Homework/hw2/Hannah_Matt_Owen/radialVelocity.jl
+++ Homework/hw2/Hannah_Matt_Owen/radialVelocity.jl
@@ -14,17 +14,62 @@ m   The mean anomaly in radians [0<m<2pi]
 Return
 f   The true anomaly [0<f<2pi] on success. -1 on failure
 =#
-function true_anomaly(e::Float64, m::Float64)
+function true_anomaly(ecc::Float64, m::Float64)
   #tan(f/2)=sqrt((1+e)/(1-e))*tan(ea/2)
-  ea = ExoJulia.Orbit.kepler_solve(m,e)
-  f = atan2(sqrt(1-e^2)*sin(ea),cos(ea)-e)
+  ea = ExoJulia.Orbit.kepler_solve(m,ecc)
+  f = atan2(sqrt(1-ecc^2)*sin(ea),cos(ea)-ecc)
 end
 
-function radial_velocity(p::Float64, mp::Float64, ms::Float64, inc::Float64,
-                         f::Float64, w::Float64, gamma::Float64)
-  #calculate the semi-amplitude k
-  k = (2pi * 6.67259E-8 / (p * (1 - e^2)^(1.5)) * (mp^5*sin(inc))^3 / (mp + ms)^2)^(1/3)
+"""
+    get_optimal_rv_parameters(data)
 
-  vRad = k*(cos(w+f)*ecc*cos(w))+gamma
+Compute the optimal period, time of periastron, and eccentricity for the given
+data.
 
+#Arguments
+* `data::Array{Float64,3}`: an Nx3 array of data with format [time; RV; error]
+"""
+function get_optimal_rv_parameters(data)
+  #TODO use Eric's algorithm to guess P
+
+  time = data[:,1];
+  rv = data[:,2];
+  err = data[:,3];
+
+  F = zeros(Float64,4,length(time)); #TODO, should 4 be passed in?
+
+  for p=100:150 #should be based on guess
+    for ecc=0:0.5
+      for tp=1:p
+        #initialize F
+        for j=1:length(time)
+          ma = 2pi/p*(time[j]-tp); #calculate the mean anomaly
+          f = true_anomaly(ecc,ma); #get the true anomaly
+          F[1,j] = cos(f);
+          F[2,j] = sin(f);
+          F[3,j] = 1;
+          F[4,j] = time[j]-time[1];
+        end
+
+        W = diagm(1./err.^2); #the error matrix
+        epsilon = inv(F * W * (F'));
+
+        #calculate beta which gives {h, c, v0, d}
+        beta = rv' * W * (F') * epsilon
+        h = beta[1]
+        c = beta[2]
+        K = sqrt(h^2+c^2)
+
+        #TODO check if this is the closest beta
+      end
+    end
+  end
 end
+
+
+
+
+
+
+data =  readdlm("./mystery_planet.txt")
+get_optimal_rv_parameters(data)
