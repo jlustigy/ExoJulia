3ce5849dcbced45932e1e5d45ef7038dba49fc5b
Owen Lehmer <Owen Lehmer>
2016-05-04 16:47:18 -0700

    hw4 update

--- Homework/hw4/Hannah_Matt_Owen/ttvs.jl
+++ Homework/hw4/Hannah_Matt_Owen/ttvs.jl
@@ -7,33 +7,74 @@ using TTVFaster
 Computer parameters for the planets passed in data.
 
 #Arguments
-* `data`: an Nx[planet transit data] array, where N is the number of planets
+* `data`: pass in any number of arrays containing transit data for each planet
 
 #Return
 * `result`: an Nx5 array where each row contains [mass_ratio, period, trans0, ecosw, esinw]
+            where N is the number of planets
 """
-function ttvs(data)
-  num_planets = size(data)[1]
+function ttvs(data...)
+  num_planets = length(data)
 
-  combined_data = [data[1];data[2]]
+  if num_planets < 2
+    println("Error: must provide data for at least 2 planets")
+    return
+  end
 
-  function model_func(useless_input_x, params)
+  combined_data = [] #combine all the data into one array, which we'll fit all at once
+  data_start_end = [] #store the start and end of each planet's data
+  for pd in data
+    combined_data = [combined_data;pd[:]]
+    if length(data_start_end) ==0
+      push!(data_start_end, ( 1,length(pd[:])) )
+    else
+      start = data_start_end[end][2]+1
+      push!(data_start_end, ( start, start+length(pd[:]) -1 ) )
+    end
+  end
+
+  function model_func(useless_input_x, params) #curve fit makes us take the first parameter
 
     planets = []
 
-    for i=1:length(params)/5.0
+    for i=1:div(length(params),5)
       planet = Planet_plane_hk(params[5*i-4],params[5*i-3],params[5*i-2],params[5*i-1],params[5*i])
       push!(planets,planet)
     end
-    ttv1 = zeros(length(data[1]))
-    ttv2 = zeros(length(data[2]))
-    compute_ttv!(5,planets[1],planets[2],data[1],data[2],ttv1,ttv2)
 
-    return [ttv1;ttv2]
+    results = zeros(length(combined_data))
+
+    for i=1:num_planets-1
+      ttv1 = zeros(data_start_end[i][2]-data_start_end[i][1] +1)
+      for j=i+1:num_planets
+        ttv2 = zeros(data_start_end[j][2]-data_start_end[j][1] +1)
+        compute_ttv!(5,planets[i],planets[j],
+            combined_data[data_start_end[i][1]:data_start_end[i][2]],
+            combined_data[data_start_end[j][1]:data_start_end[j][2]],
+            ttv1,ttv2);
+        results[data_start_end[i][1]:data_start_end[i][2]] = results[data_start_end[i][1]:data_start_end[i][2]] + ttv1
+        results[data_start_end[j][1]:data_start_end[j][2]] = results[data_start_end[j][1]:data_start_end[j][2]] + ttv2
+      end
+    end
+
+    #convert the results array from ttvs back to periods for curve fit to compare against
+    for i=1:num_planets
+      d_start = data_start_end[i][1]
+      d_end = data_start_end[i][2]
+      for j=d_start:d_end
+        results[j] = results[j] + (j - d_start)*planets[i].period + planets[i].trans0
+      end
+    end
+
+    println("------------------------")
+    println(results[data_start_end[1][1]:data_start_end[1][2]])
+    println(size(results))
+    println(typeof(results))
+    return results
   end
 
   p0 = [0.000003, 225, 8445, 0.0001, 0.0001, 0.000003, 365, 8461, 0.0001, 0.0001]
-  fit = curve_fit(model_func, [], data, p0)
+  fit = curve_fit(model_func, [], combined_data, p0)
 
   return fit.param
 end
@@ -47,7 +88,7 @@ function test()
   push!(data, p1[:,1])
   push!(data, p2[:,1])
 
-  result = ttvs(data)
+  result = ttvs(p1,p2)
   return result
 end
 
