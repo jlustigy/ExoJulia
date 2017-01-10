8b1d44fe0e4a73919ecd0a05505d162880c1357a
Tyler Gordon <tagordon@uw.edu>
2017-01-09 18:45:04 -0800

    added newton-raphson solver and changed test script

--- winter17/hw1/gordon/KeplerSolver.jl
+++ winter17/hw1/gordon/KeplerSolver.jl
@@ -9,8 +9,19 @@ function E(ecc, M)
   tor = zeros(length(M))
   for (i, m) in enumerate(M)
     f(x) = x - ecc * sin(x) - m
-    tor[i] = fzero(f, m)
+    fp(x) = 1 - ecc * cos(x)
+    tor[i] = nsolve(f, fp, m, 1e-6)
   end
   return tor
+end
+
+function nsolve(f, fp, x, tolerance)
+  delta = tolerance + 1
+  while delta > tolerance
+    last = x
+    x = x - f(x)/fp(x)
+    delta = abs(x - last)
+  end
+  return x
 end
 end
