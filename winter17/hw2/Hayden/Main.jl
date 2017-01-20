a8f3ba4eb0ab481a212115d8426ce0df75a77bb9
Hayden <hayden.smotherman@outlook.com>
2017-01-20 15:56:41 -0800

    Hayden's HW 2 -- Better data

--- winter17/hw2/Hayden/Main.jl
+++ winter17/hw2/Hayden/Main.jl
@@ -3,7 +3,14 @@
 
 include("RVFit.jl")
 tic()
-BestFit=RVFit("mystery_planet.txt",[300, 1.9*pi, 200, 71, -10, 0.3])
+BestFit,Residual=RVFit("mystery_planet.txt",[300, 1.9*pi, 200, 71, -10, 0.3],300);
 toc()
 
-#Runs in ~0.5 seconds w/ plotting. ~0.25 w/o plotting.
+println("Semiamplitude (K) = $(BestFit[1])")
+println("Longitude of Pericenter = $(BestFit[2])")
+println("Period (P) = $(BestFit[3])")
+println("Time of Pericenter Passage = $(BestFit[4])")
+println("Gamma = $(BestFit[5])")
+println("Eccentricity = $(BestFit[6])")
+println("R^2 = $(sum(Residual.^2))")
+#Runs in ~0.5 seconds w/ plotting. ~0.25 w/o plotting. (For 10 iterations)
--- winter17/hw2/Hayden/RVFit.jl
+++ winter17/hw2/Hayden/RVFit.jl
@@ -1,15 +1,15 @@
 #HW 1b Problem 2
 #===================
 FUNCTION NAME: RV_Fit
-INPUTS:   FileName, InitialGuess [K, w, P, tp, gamma, ecc]
-OUTPUTS:  ?
+INPUTS:   FileName, InitialGuess [K, w, P, tp, gamma, ecc], NumOfIterations
+OUTPUTS:  Parameters for the best fit RV values, Residual for the fit
 ===================#
 using LsqFit
 using PyPlot
 include("RVSolver.jl")
 include("NewtonKeplerSolver.jl")
 
-function RVFit(FileName, InitialGuess)
+function RVFit(FileName, InitialGuess, NumOfIterations)
   #Read the data in from the text file
   DATA = readdlm(FileName)
   t   = DATA[:,1]; #Time
@@ -22,9 +22,9 @@ function RVFit(FileName, InitialGuess)
   #Fold the data based on the guessed period
   for j = 1:100
   #Iteratively select smaller and smaller sampling ranges
-  PeriodRange = linspace((1-0.9/j)*InitialGuess[3],(1+0.9/j)*
-    InitialGuess[3],10);
-  for i=1:10 #Sample the given period space
+  PeriodRange = linspace((1-0.9/j)*InitialGuess[3],(1+0.5/j)*
+    InitialGuess[3],NumOfIterations);
+  for i=1:NumOfIterations #Sample the given period space
     t = DATA[:,1] - minimum(DATA[:,1]); #Reset t with the first data point being zero
     while maximum(t)>PeriodRange[i] #Fold time based on the guessed period
       t[t.>PeriodRange[i]]-=PeriodRange[i]
@@ -44,7 +44,7 @@ function RVFit(FileName, InitialGuess)
   PlotDomain = linspace(minimum(Bestt),maximum(Bestt),1000);
   plot(PlotDomain, RVSolver(PlotDomain,fit.param))
   ylabel("Radial Velocity m/s")
-  xlabel("Time")
+  xlabel("Time days")
   title("Radial Velocity best-fit")
-  return fit.param
+  return fit.param, fit.resid
 end
